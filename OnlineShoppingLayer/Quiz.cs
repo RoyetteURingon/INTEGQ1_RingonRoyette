ac0463a3c76686914c22314932502fd38fea449a
RoyetteURingon <81809097+RoyetteURingon@users.noreply.github.com>
2021-06-27 19:34:07 +0800

    Add files via upload

---
+++ OnlineShoppingLayer/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DL_layer;
+using BL_Layer;
+
+namespace OnlineShoppingLayer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BL login = new BL ();
+            login.log();
+            
+        }
+    }
+}
